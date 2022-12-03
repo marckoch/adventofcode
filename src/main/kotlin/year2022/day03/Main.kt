55fbae2d45f78c3e831def93def347bf1bbbba51
Marc Koch <marckoch@users.noreply.github.com>
2022-12-03 09:42:48 +0100

    solve 2022.3

---
+++ src/main/kotlin/year2022/day03/Main.kt
@@ -0,0 +1,45 @@
+package year2022.day03
+
+fun main() {
+    part1()
+
+    part2()
+}
+
+fun part1() {
+    INPUT.lines()
+        .map { it.chunked(it.length / 2) }
+        .map { findCommonCharIn(it) }
+        .sumOf { itemPriorityOf(it) }
+        .let { println(it) }
+}
+
+fun part2() {
+    INPUT.lines()
+        .chunked(3)
+        .map { findCommonCharIn(it) }
+        .sumOf { itemPriorityOf(it) }
+        .let { println(it) }
+}
+
+fun findCommonCharIn(strings: List<String>): Char {
+    val common = strings.fold(charsOf(strings[0])) { acc, i -> (acc intersect charsOf(i)).toList() }
+    assert(common.size == 1)
+    return common.first()
+}
+
+fun itemPriorityOf(c: Char): Int {
+    when (c) {
+        in 'a'..'z' -> return c.code - 97 + 1   // 'a' == 97 in ASCII
+        in 'A'..'Z' -> return c.code - 65 + 27  // 'A' == 65 in ASCII
+    }
+    throw IllegalArgumentException("$c is invalid argument")
+}
+
+// convert weird IntStream that 'chars()' returns to nice List<Char>
+fun charsOf(s: String): List<Char> {
+    return s.chars()
+        .boxed()
+        .map { it.toChar() }
+        .toList()
+}
