4d3bc9eac3e7b18de4dcf3f2c8a33216829b90b4
Marc Koch <marckoch@users.noreply.github.com>
2022-12-04 09:41:37 +0100

    solve 2022.4

---
+++ src/main/kotlin/year2022/day04/Main.kt
@@ -0,0 +1,39 @@
+package year2022.day04
+
+fun main() {
+    part1()
+
+    part2()
+}
+
+fun part1() {
+    INPUT.lines()
+        .count { oneAreaCoversOtherArea(it) }
+        .let { println(it) }
+}
+
+fun part2() {
+    INPUT.lines()
+        .count { hasOverlap(it) }
+        .let { println(it) }
+}
+
+fun oneAreaCoversOtherArea(line: String): Boolean {
+    val (start1, end1, start2, end2) = extractIntsFrom(line)
+
+    val firstCoversSecond = (start1 <= start2 && end2 <= end1)
+    val secondCoversFirst = (start2 <= start1 && end1 <= end2)
+
+    return firstCoversSecond || secondCoversFirst
+}
+
+fun hasOverlap(line: String): Boolean {
+    val (start1, end1, start2, end2) = extractIntsFrom(line)
+
+    val firstBeforeSecond = end1 < start2   // without overlap
+    val firstAfterSecond = end2 < start1    // without overlap
+
+    return !(firstBeforeSecond || firstAfterSecond)
+}
+
+fun extractIntsFrom(s: String) = s.split(",", "-").map { it.toInt() }
