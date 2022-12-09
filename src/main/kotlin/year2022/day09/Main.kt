3b7dcc1e5c58527b9a724c27d1c32e52f8d6d8e1
Marc Koch <marckoch@users.noreply.github.com>
2022-12-09 15:40:28 +0100

    solve 2022.9

--- src/main/kotlin/year2022/day09/Main.kt
+++ src/main/kotlin/year2022/day09/Main.kt
@@ -6,98 +6,44 @@ import kotlin.math.min
 import kotlin.math.sign
 
 fun main() {
-    part1(SAMPLE)
-    part1(INPUT)
+    println("part1: " + solve(SAMPLE, 2))
+    println("part1: " + solve(INPUT, 2))
 
-//    part2(SAMPLE)
-//    part2(INPUT) // 5447 too high
-}
-
-enum class Dir {
-    R, L, U, D
+    println("part2: " + solve(SAMPLE, 10))
+    println("part2: " + solve(SAMPLE2, 10))
+    println("part2: " + solve(INPUT, 10))
 }
 
 val LINE_PATTERN = """([RLUD]) (\d+)""".toRegex()
-fun part1(input: String) {
-    var headPos = Pair(0, 0)
-    var tailPos = Pair(0, 0)
-    val tailVisits = mutableSetOf<Pos>()
-    tailVisits.add(tailPos)
 
-    input.lines().forEach {
-        val (s1, s2) = LINE_PATTERN.find(it)!!.destructured
-        val direction = Dir.valueOf(s1)
-        val numberOfSteps = s2.toInt()
-
-//        println(it)
-
-        repeat(numberOfSteps) {
-            headPos = moveHead(direction, headPos)
-            tailPos = moveNextSegment(headPos, tailPos)
-            tailVisits.add(tailPos)
-            //print(headPos, tailPos)
-        }
-    }
-    println("part1: ${tailVisits.size}")
-}
-
-fun moveNextSegment(head: Pos, tail: Pos): Pos {
-    val dX = head.first - tail.first
-    val dY = head.second - tail.second
-    if (abs(dX) > 1) {
-        return Pos(tail.first + dX.sign, head.second)
-    }
-    if (abs(dY) > 1) {
-        return Pos(head.first, tail.second + dY.sign)
-    }
-
-    // don't move
-    return tail
-}
-
-fun part2(input: String) {
+fun solve(input: String, ropeLength: Int): Int {
     val segments = mutableListOf<Pos>()
-    segments.addAll(List(10) { Pos(0, 0) })
+    segments.addAll(List(ropeLength) { Pos(0, 0) })
+
     val tailVisits = mutableSetOf<Pos>()
-    tailVisits.add(Pair(0, 0))
+    tailVisits.add(Pos(0, 0))
 
     input.lines().forEach {
         val (s1, s2) = LINE_PATTERN.find(it)!!.destructured
         val direction = Dir.valueOf(s1)
         val numberOfSteps = s2.toInt()
 
-        println(it)
-
         repeat(numberOfSteps) {
-            var oldHeadPos = segments[0]
-            segments[0] = moveHead(direction, oldHeadPos)
+//            println("$direction ${it+1}/$numberOfSteps")
+            segments[0] = moveHead(direction, segments[0])
 
-            for (i in (0..segments.lastIndex).windowed(2)) {
-                val h = i[0]
-                val t = i[1]
-                if (nextShouldFollow(segments[h], segments[t])) {
-                    val oldTailPos = segments[t]
-                    segments[t] = oldHeadPos
-                    oldHeadPos = oldTailPos
-                }
+            // for the rest of the line each segment acts as the head for its follower
+            for (i in segments.indices.windowed(2)) {
+                val headIndex = i[0]
+                val tailIndex = i[1]
+                segments[tailIndex] = moveNextSegment(segments[headIndex], segments[tailIndex])
             }
+
             tailVisits.add(segments.last())
-//            println(segments)
-            print(segments)
+            //print(segments)
         }
     }
-    println("part1: ${tailVisits.size}")
-}
-
-// next segment should follow if current one is more than 1 field away,
-// in corner case tail should NOT follow
-// e.g.
-// ..H
-// .T.
-fun nextShouldFollow(head: Pos, tail: Pos): Boolean {
-    if (abs(head.first - tail.first) > 1) return true
-    if (abs(head.second - tail.second) > 1) return true
-    return false
+    return tailVisits.size
 }
 
 fun moveHead(dir: Dir, x: Pos): Pos {
@@ -109,32 +55,27 @@ fun moveHead(dir: Dir, x: Pos): Pos {
     }
 }
 
-fun moveUp(pos: Pos): Pos = Pair(pos.first, pos.second + 1)
-fun moveDown(pos: Pos): Pos = Pair(pos.first, pos.second - 1)
-fun moveLeft(pos: Pos): Pos = Pair(pos.first - 1, pos.second)
-fun moveRight(pos: Pos): Pair<Int, Int> = Pair(pos.first + 1, pos.second)
+fun moveNextSegment(head: Pos, tail: Pos): Pos {
+    val dX = head.first - tail.first
+    val dY = head.second - tail.second
+
+    if (abs(dX) > 1 || abs(dY) > 1) {
+        return Pos(tail.first + dX.sign, tail.second + dY.sign)
+    }
+
+    // don't move
+    return tail
+}
+
+fun moveUp(pos: Pos): Pos = Pos(pos.first, pos.second + 1)
+fun moveDown(pos: Pos): Pos = Pos(pos.first, pos.second - 1)
+fun moveLeft(pos: Pos): Pos = Pos(pos.first - 1, pos.second)
+fun moveRight(pos: Pos): Pos = Pos(pos.first + 1, pos.second)
 
 typealias Pos = Pair<Int, Int>
 
-private fun print(head: Pos, tail: Pos) {
-    println()
-    val fromX = listOf(0, head.first, tail.first).min()
-    val toX = listOf(0, head.first, tail.first).max()
-    val fromY = listOf(0, head.second, tail.second).min()
-    val toY = listOf(0, head.second, tail.second).max()
-
-    (toY downTo fromY).forEach { y ->
-        val r = (fromX..toX).map { x ->
-            if (x == head.first && y == head.second) {
-                "H"
-            } else if (x == tail.first && y == tail.second) {
-                "T"
-            } else if (x == 0 && y == 0) {
-                "s"
-            } else "."
-        }
-        println(r.joinToString(""))
-    }
+enum class Dir {
+    R, L, U, D
 }
 
 private fun print(segments: List<Pos>) {
@@ -146,7 +87,8 @@ private fun print(segments: List<Pos>) {
 
     (toY downTo fromY).forEach { y ->
         val r = (fromX..toX).map { x ->
-            val s = segments.withIndex().firstOrNull { (index, pair) -> pair.first == x && pair.second == y }
+            // check if we have a segment on this field
+            val s = segments.withIndex().firstOrNull { (_, pair) -> pair.first == x && pair.second == y }
             if (s != null) {
                 if (s.index == 0) "H" else s.index.toString()
             } else {
