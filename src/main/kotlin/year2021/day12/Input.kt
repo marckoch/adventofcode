b5731c94385a7195504e87493b2e496987b3443b
Marc Koch <marckoch@users.noreply.github.com>
2022-12-10 22:42:13 +0100

    solve 2021.12 - part 1 works

---
+++ src/main/kotlin/year2021/day12/Input.kt
@@ -0,0 +1,62 @@
+package year2021.day12
+
+const val SAMPLE1 = """start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end"""
+
+const val SAMPLE2 = """dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc"""
+
+const val SAMPLE3 = """fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW"""
+
+const val INPUT = """um-end
+pk-um
+FE-il
+ay-FE
+pk-start
+end-jt
+um-FE
+RO-il
+xc-ay
+il-end
+start-EZ
+pk-FE
+xc-start
+jt-FE
+EZ-um
+pk-xc
+xc-EZ
+pk-ay
+il-ay
+jt-EZ
+jt-om
+pk-EZ"""
---
+++ src/main/kotlin/year2021/day12/Main.kt
@@ -0,0 +1,75 @@
+package year2021.day12
+
+fun main() {
+    part1(SAMPLE1)
+    part1(SAMPLE2)
+    part1(SAMPLE3)
+    part1(INPUT)
+}
+
+fun part1(input: String) {
+    val connections = input.lines().map {
+        val (from, to) = "(\\w*)-(\\w*)".toRegex().find(it)!!.destructured
+        Pair(from, to)
+    }
+
+    val graph = buildGraph(connections)
+
+    val paths = dfs(graph, "start", "end")
+//    paths.forEach { println(it) }
+    println(paths.size)
+}
+
+fun buildGraph(connections: List<Pair<String, String>>): Map<String, List<String>> {
+    return connections.fold(HashMap<String, MutableList<String>>()) { acc, pair ->
+        val from = pair.first
+        val to = pair.second
+
+        if (acc.containsKey(from)) {
+            acc[from]!!.add(to)
+        } else {
+            acc[from] = mutableListOf(to)
+        }
+
+        if (acc.containsKey(to)) {
+            acc[to]!!.add(from)
+        } else {
+            acc[to] = mutableListOf(from)
+        }
+
+        acc
+    }
+}
+
+fun dfs(g: Map<String, List<String>>, from: String, to: String): List<List<String>> {
+    val paths = mutableListOf<List<String>>()
+    val visited = mutableListOf<String>()
+    dfs(g, from, to, ArrayDeque(), paths, visited)
+    return paths
+}
+
+// https://www.baeldung.com/cs/simple-paths-between-two-vertices
+fun dfs(g: Map<String, List<String>>, from: String, to: String, currentPath: ArrayDeque<String>, paths: MutableList<List<String>>, visited: MutableList<String>) {
+    val fromIsSmallCave = from.all { it in ('a'..'z') }
+    if (visited.contains(from)) {
+        return
+    }
+    if (fromIsSmallCave)
+        visited.add(from)
+    currentPath.addLast(from)
+    if (from == to) {
+        paths.add(currentPath.toMutableList()) // make copy by calling toMutableList
+        if (fromIsSmallCave)
+            visited.remove(from)
+        currentPath.removeLast()
+        return
+    }
+
+    for (next in g[from]!!.sorted()) {
+        dfs(g, next, to, currentPath, paths, visited)
+    }
+
+    if (fromIsSmallCave)
+        visited.remove(from)
+    currentPath.removeLast()
+}
