662086d89e7d66fa5f3aadf63d44e909e991d224
Marc Koch <marckoch@users.noreply.github.com>
2023-12-11 08:22:35 +0100

    solve 2023.11

--- src/main/kotlin/year2023/day11/Main.kt
+++ src/main/kotlin/year2023/day11/Main.kt
@@ -1,19 +1,38 @@
 package year2023.day11
 
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
     part1(SAMPLE_1_1).let { println(it) }  // 374
     part1(INPUT).let { println(it) }  // 9274989
+
+    part2(SAMPLE_1_1, 2).let { println(it) }  // 374
+    part2(INPUT, 2).let { println(it) }  // 9274989
+    part2(SAMPLE_1_1, 10).let { println(it) } // 1030
+    part2(SAMPLE_1_1, 100).let { println(it) } // 8410
+    part2(INPUT, 1_000_000).let { println(it) } // 357134560737
 }
 
 fun part1(input: String): Int {
+    // old school: add empty rows and cols to input
     val rows = input.lines().drop(1)
     val rows1 = duplicateEmptyRows(rows)
     val rows2 = duplicateEmptyCols(rows1)
 
     val galaxies = extractGalaxies(rows2)
     return pairUp(galaxies).sumOf { (g1, g2) -> manhattanDistance(g1, g2) }
+}
+
+fun part2(input: String, expansionFactor: Int): Long {
+    // a bit smarter: check which rows and cols are empty and pass those to our distance function
+    val rows = input.lines().drop(1)
+    val rowsWithNoGalaxy = rowsWithNoGalaxy(rows)
+    val colsWithNoGalaxy = colsWithNoGalaxy(rows)
+
+    val galaxies = extractGalaxies(rows)
+    return pairUp(galaxies).sumOf { (g1, g2) -> manhattanDistance_new(g1, g2, rowsWithNoGalaxy, colsWithNoGalaxy, expansionFactor) }
 }
 
 fun duplicateEmptyRows(rows: List<String>): List<String> {
@@ -37,6 +56,16 @@ fun duplicateEmptyCols(rows: List<String>): List<String> {
         }
     }
     return acc.map { it.joinToString("") }
+}
+
+fun rowsWithNoGalaxy(rows: List<String>): List<Int> {
+    return rows.withIndex().filter { !it.value.contains('#') }.map { it.index }
+}
+
+fun colsWithNoGalaxy(rows: List<String>): List<Int> {
+    return (0..rows.first().lastIndex).filter { c ->
+        !rows.any { it[c] == '#' }
+    }
 }
 
 fun extractGalaxies(rows: List<String>): List<Galaxy> {
@@ -65,6 +94,15 @@ fun manhattanDistance(galaxy1: Galaxy, galaxy2: Galaxy): Int {
     val (r1, c1) = galaxy1
     val (r2, c2) = galaxy2
     return abs(r1 - r2) + abs(c1 - c2)
+}
+
+fun manhattanDistance_new(galaxy1: Galaxy, galaxy2: Galaxy, rowsWithNoGalaxy: List<Int>, colsWithNoGalaxy: List<Int>, expansionFactor: Int): Long {
+    val (r1, c1) = galaxy1
+    val (r2, c2) = galaxy2
+    return abs(r1 - r2).toLong() +
+            rowsWithNoGalaxy.count { it in min(r1, r2) + 1 until max(r1, r2) } * (expansionFactor - 1) +
+            abs(c1 - c2) +
+            colsWithNoGalaxy.count { it in min(c1, c2) + 1 until max(c1, c2) } * (expansionFactor - 1)
 }
 
 typealias Galaxy = Pair<Int, Int>
--- src/test/kotlin/year2023/day11/Year2023Day11Test.kt
+++ src/test/kotlin/year2023/day11/Year2023Day11Test.kt
@@ -15,4 +15,29 @@ class Year2023Day11Test {
     fun `part 1`() {
         assertThat(part1(INPUT)).isEqualTo(9274989)
     }
+
+    @Test
+    fun `part 2 sample 1 factor 2`() {
+        assertThat(part2(SAMPLE_1_1, 2)).isEqualTo(374)
+    }
+
+    @Test
+    fun `part 2 sample 1 factor 10`() {
+        assertThat(part2(SAMPLE_1_1, 10)).isEqualTo(1030)
+    }
+
+    @Test
+    fun `part 2 sample 1 factor 100`() {
+        assertThat(part2(SAMPLE_1_1, 100)).isEqualTo(8410)
+    }
+
+    @Test
+    fun `part 2 factor 2`() {
+        assertThat(part2(INPUT, 2)).isEqualTo(9274989)
+    }
+
+    @Test
+    fun `part 2 factor 1_000_000`() {
+        assertThat(part2(INPUT, 1_000_000)).isEqualTo(357134560737)
+    }
 }
