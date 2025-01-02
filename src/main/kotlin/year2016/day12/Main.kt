9e0857c264af0a7d468916e480bf533884ed9847
Marc Koch <marckoch@users.noreply.github.com>
2025-01-02 12:48:39 +0100

    refactor 2016.12

--- src/main/kotlin/year2016/day12/Main.kt
+++ src/main/kotlin/year2016/day12/Main.kt
@@ -9,74 +9,44 @@ fun main() {
 }
 
 class AOC2016D12(input: String) : Problem(input) {
-    var a = 0
-    var b = 0
-    var c = 0
-    var d = 0
+    override fun solve1() = execute(State(a = 0, b = 0, c = 0, d = 0, pos = 0))
+    override fun solve2() = execute(State(a = 0, b = 0, c = 1, d = 0, pos = 0))
 
-    override fun solve1() = execute(0)
-    override fun solve2() = execute(1)
-
-    private fun execute(initialC: Int): Int {
-        // reset state to initial values before run
-        a = 0
-        b = 0
-        c = initialC
-        d = 0
-
-        var pos = 0
-
+    private fun execute(initialState: State): Int {
         val instructions = inputLines().map { parseInstruction(it) }
-        while (pos in instructions.indices) {
-            val (operation, register, newValue, offset) = instructions[pos]
-            when (operation) {
-                Instruction.CPY -> {
-                    modifyRegister(register) { getValue(newValue) }
-                    pos++
-                }
-                Instruction.INC -> {
-                    modifyRegister(register, Int::inc)
-                    pos++
-                }
-                Instruction.DEC -> {
-                    modifyRegister(register, Int::dec)
-                    pos++
-                }
-                Instruction.JNZ -> if (getValue(register) != 0) pos += offset!! else pos++
-            }
+
+        var currentState = initialState
+        while (currentState.pos in instructions.indices) {
+            val instruction = instructions[currentState.pos]
+            currentState = instruction.modify(currentState)
         }
-        return a
+        return currentState.a
     }
 
-    fun getValue(v: String?): Int {
-        return if (v!!.isNumber()) {
-             v.toInt()
-        } else {
-             getRegisterValue(v)
-        }
-    }
-
-    private fun parseInstruction(line: String): InstructionData {
+    private fun parseInstruction(line: String): Instruction {
         val parts = line.split(" ")
-        when (val operation = Instruction.valueOf(parts[0].uppercase())) {
-            Instruction.CPY -> {
-                val newValue = parts[1]
-                val register = parts[2]
-                return InstructionData(operation, register, newValue, null)
-            }
-            Instruction.INC, Instruction.DEC -> {
-                val register = parts[1]
-                return InstructionData(operation, register, null, null)
-            }
-            Instruction.JNZ -> {
-                val register = parts[1]
-                val offset = parts[2].toInt()
-                return InstructionData(operation, register, null, offset)
-            }
+        return when (parts[0]) {
+            "cpy" -> Copy(source = parts[1], register = parts[2])
+            "inc" -> Inc(register = parts[1])
+            "dec" -> Dec(register = parts[1])
+            "jnz" -> JumpNotZero(register = parts[1], offset = parts[2].toInt())
+            else -> error("Unknown operation in line: $line")
+        }
+    }
+}
+
+data class State(val a: Int, val b: Int, val c: Int, val d: Int, val pos: Int) {
+    fun modifyRegister(register: String, operation: (Int) -> Int): State {
+        return when (register) {
+            "a" -> this.copy(a = operation(a))
+            "b" -> this.copy(b = operation(b))
+            "c" -> this.copy(c = operation(c))
+            "d" -> this.copy(d = operation(d))
+            else -> throw IllegalArgumentException("Unknown register: $register")
         }
     }
 
-    private fun getRegisterValue(register: String?): Int {
+    private fun getRegisterValue(register: String): Int {
         return when (register) {
             "a" -> a
             "b" -> b
@@ -86,23 +56,41 @@ class AOC2016D12(input: String) : Problem(input) {
         }
     }
 
-    private fun modifyRegister(register: String?, operation: (Int) -> Int) {
-        when (register) {
-            "a" -> a = operation(a)
-            "b" -> b = operation(b)
-            "c" -> c = operation(c)
-            "d" -> d = operation(d)
+    fun getValue(v: String): Int {
+        return if (v.isNumber()) {
+            v.toInt()
+        } else {
+            getRegisterValue(v)
         }
     }
 
-    private data class InstructionData(
-        val operation: Instruction,
-        val register: String?,
-        val newValue: String?,
-        val offset: Int?
-    )
+    fun incPos(offset: Int = 1) = this.copy(pos = pos + offset)
+}
 
-    private enum class Instruction {
-        CPY, INC, DEC, JNZ
+interface Instruction {
+    fun modify(state: State): State
+}
+
+class Copy(private val source: String, private val register: String) : Instruction {
+    override fun modify(state: State): State {
+        return state.modifyRegister(register) { state.getValue(source) }.incPos()
+    }
+}
+
+class Inc(private val register: String) : Instruction {
+    override fun modify(state: State): State {
+        return state.modifyRegister(register, Int::inc).incPos()
+    }
+}
+
+class Dec(private val register: String) : Instruction {
+    override fun modify(state: State): State {
+        return state.modifyRegister(register, Int::dec).incPos()
+    }
+}
+
+class JumpNotZero(private val register: String, private val offset: Int) : Instruction {
+    override fun modify(state: State): State {
+        return state.incPos(offset = if (state.getValue(register) != 0) offset else 1)
     }
 }
